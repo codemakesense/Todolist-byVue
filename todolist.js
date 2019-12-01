37369f62ad14533a877b33ef75f71887b6aaf67e
Simon-Lvliang <346207308@qq.com>
2019-12-01 11:36:07 +0800

    实现了不同分组的动态组件，遗留一个BUG：切换组件的时候状态不能保留，用keep-alive无法实现v-for指令

--- todolist.js
+++ todolist.js
@@ -1,5 +1,5 @@
-// 全局注册 todo-list 组件
-Vue.component("todo-list", {
+// 全局注册 todo-list-work 组件
+Vue.component("todo-list-work", {
     // 传入app模块的list数组和deltodo方法
     props: ["list", "deltodo"],
     // 初始化待办事项是否可见的checkbox的属性，ischecked
@@ -44,6 +44,55 @@ Vue.component("todo-list", {
     }
     
 })
+
+// 全局注册 todo-list-learn 组件
+Vue.component("todo-list-learn", {
+    // 传入app模块的list数组和deltodo方法
+    props: ["list", "deltodo"],
+    // 初始化待办事项是否可见的checkbox的属性，ischecked
+    data: function () {
+        return {
+            ischecked: false,
+        }
+    },
+    // 设置组件模板 
+    // 点击checkbox时，调用getCheckedValue函数
+    // 将list.title所在的span元素的class是否生效为与ischecked关联，
+    // 点击删除按钮时调用Vue实例中的deltodo方法并传入两个参数，当前的list以及对应的list.index
+    template: `
+        <div class="content-list">
+            <div class="finish-input">
+                <input type="checkbox"
+                    v-on:click="getCheckedValue($event.target.checked)"
+                >
+            </div>
+            <div
+                v-bind:class="{ 'todo-list-fin': ischecked }"
+                class="todo-list"
+            >{{ list.title }}</div>
+            <div class="del-button">
+                <button
+                    v-on:click="deltodo(list, list.index)"
+                >删除</button>
+            </div>
+        </div>
+    `,
+    methods: {
+        // 定义开关checked方法，点击一次则变更ischecked的boolean属性
+        getCheckedValue (checked) {
+            if (checked == true) {
+                this.ischecked = true
+                this.list.isfinished = true
+            } else if (checked == false) {
+                this.ischecked = false
+                this.list.isfinished = false
+            }
+        },
+    }
+    
+})
+
+
 // 注册 add-todolist 组件
 Vue.component("add-todolist", {
     // 传入Vue实例的 addtodo方法
@@ -92,13 +141,24 @@ var todoContent = new Vue({
 
     // 初始化一些变量
     data: {
-        todoLists: [
-            {key: 0, index: 0, title: "定一个写Vue应用的小目标", visible: true, isfinished: false },
-            {key: 1, index: 1, title: "定一个晚上12点前睡觉的小目标", visible: true, isfinished: false },
-            {key: 2, index: 2, title: "定一个明年2月份前换工作的小目标", visible: true, isfinished: false },
-            {key: 3, index: 3, title: "定一个学会Vue、ES6和Node.js技术栈的小目标", visible: true, isfinished: false },
-            {key: 4, index: 4, title: "这个应用已经阶段性完成，后面继续调试即可", visible: true, isfinished: false },
+        todoWorkLists: [
+            {key: 0, index: 0, visible: true, type: "work", isfinished: false, title: "定一个写Vue应用的小目标" },
+            {key: 1, index: 1, visible: true, type: "work", isfinished: false, title: "定一个晚上12点前睡觉的小目标" },
+            {key: 2, index: 2, visible: true, type: "work", isfinished: false, title: "定一个明年2月份前换工作的小目标",  },
+            {key: 3, index: 3, visible: true, type: "work", isfinished: false, title: "定一个学会Vue、ES6和Node.js技术栈的小目标",  },
+            {key: 4, index: 4, visible: true, type: "work", isfinished: false, title: "这个应用已经阶段性完成，后面继续调试即可",  },
+            {key: 5, index: 5, visible: true, type: "learn", isfinished: false, title: "学点有用的东西",  },
+            {key: 6, index: 6, visible: true, type: "learn", isfinished: false, title: "学点有用的东西",  },
+            {key: 7, index: 7, visible: true, type: "learn", isfinished: false, title: "学点有用的东西",  },
+            {key: 8, index: 8, visible: true, type: "learn", isfinished: false, title: "学点有用的东西",  },
+            {key: 9, index: 9, visible: true, type: "learn", isfinished: false, title: "学点有用的东西",  },
         ],
+        currentLists: [],
+        tabs: [
+            {type: "work", name: "工作"},
+            {type: "learn", name: "学习"}
+        ],
+        currentTab: "work",
         newTodo: "",    // 接收输入框的新待办事项
         deleteIndex: Number // 定义删除事项在数组的序号的变量类型
     },
@@ -109,10 +169,10 @@ var todoContent = new Vue({
             // 先将li元素是否可见设为false
             arr.visible = false
             // 修改数组，删除传入排序为indx参数的数组项
-            this.todoLists.splice(indx, 1)
+            this.currentLists.splice(indx, 1)
             // 重新遍历一次数组，将每个数组项的index属性初始化（即从头排序）
-            for (var i = 0; i < this.todoLists.length; i++) {
-                this.todoLists[i].index = i
+            for (var i = 0; i < this.currentLists.length; i++) {
+                this.currentLists[i].index = i
             }
         },
         // 增加待办事项函数
@@ -122,25 +182,38 @@ var todoContent = new Vue({
                 // 为空则提出提示
                 alert("请输入有效的待办事项")
               // 插入到数组中，同时将key和index按数组长度顺序赋值  
-            } else this.todoLists.push({key: this.todoLists.length, index: this.todoLists.length, title: addItem, visible: true, isfinished: false})
+            } else this.todoWorkLists.push({key: this.todoWorkLists.length, index: this.currentLists.length, type: this.currentTab, title: addItem, visible: true, isfinished: false})
         },
-        
+        /*changeTodolists: function () {
+            this.currentTab = this.tabs.type
+            this.currentLists = 
+        }*/
     },
 
     computed: {
         // 实时统计数组中待办项的总数
         countList: function () {
-            var count = this.todoLists.length
+            var count = this.currentLists.length
             return count
         },
         countUnfinished () {
             var count = 0
-            for (let i = 0; i < this.todoLists.length; i++) {
-                if (this.todoLists[i].isfinished == false) {
+            for (let i = 0; i < this.currentLists.length; i++) {
+                if (this.currentLists[i].isfinished == false) {
                     count = count + 1
                 }
             }
             return count
+        },
+        currentTabComponent: function () { 
+            return "todo-list-" + this.currentTab.toLowerCase()
+        },
+        getTypeLists: function () {
+            return this.currentLists = this.todoWorkLists.filter(item => item.type == this.currentTab)
         }
-    }
+    },
+
+    /*created: function () {
+        this.currentLists = this.todoWorkLists
+    }*/
 })
